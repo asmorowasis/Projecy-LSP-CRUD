b3235e016cb016d4ad3f3e190927c7eb4659681d
asmorowasis <67616331+asmorowasis@users.noreply.github.com>
2022-07-03 15:22:36 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,66 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+	<h5 class="my-0 mr-md-auto font-weight-normal">Ujian PWEB</h5>
+	<nav class="my-2 my-md-0 mr-md-3">
+		<a class="p-2 text-dark" href="#">Tambah Data</a>
+	</nav>
+	<a class="btn btn-outline-primary" href="#">Sign Out</a>
+</div>
+
+<div class="container">
+	<div class="row justify-content-md-center">
+		<div class="col-md-6">
+			<div class="card bg-light my-5">
+				<div class="card-header text-center">Tambah Data</div>
+				<div class="card-body">
+					<form action="" method="post" class="needs-validation" novalidate>
+						<div class="form-group">
+							<label for="nama_mhs">Nama Santri</label>
+							<input type="text" class="form-control" name="nama_mhs" id="nama_mhs" placeholder="Masukan nama santri" autocomplete="off" required>
+							<div class="invalid-feedback">
+								Anda belum memasukan nama santri.
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="n_pert1">Nilai Baca Quran</label>
+							<input type="n_pert1" class="form-control" name="n_pert1" id="n_pert1" placeholder="Masukan nilai baca quran" autocomplete="off" required>
+							<div class="invalid-feedback">
+								Anda belum memasukan nilai baca quran.
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="n_pert2">Nilai Hafalan</label>
+							<input type="n_pert2" class="form-control" name="n_pert2" id="n_pert2" placeholder="Masukan nilai hafalan" autocomplete="off" required>
+							<div class="invalid-feedback">
+								Anda belum memasukan nilai hafalan.
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="n_pert3">Nilai Baca Tulis Arab</label>
+							<input type="n_pert3" class="form-control" name="n_pert3" id="n_pert3" placeholder="Masukan nilai baca tulis arab" autocomplete="off" required>
+							<div class="invalid-feedback">
+								Anda belum memasukan nilai baca tulis arab.
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="n_pert4">Nilai Akhlak</label>
+							<input type="n_pert4" class="form-control" name="n_pert4" id="n_pert4" placeholder="Masukan nilai akhlak" autocomplete="off" required>
+							<div class="invalid-feedback">
+								Anda belum memasukan nilai akhlak.
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="n_pert5">Nilai Ibadah</label>
+							<input type="n_pert5" class="form-control" name="n_pert5" id="n_pert5" placeholder="Masukan nilai ibadah" autocomplete="off" required>
+							<div class="invalid-feedback">
+								Anda belum memasukan nilai ibadah.
+							</div>
+						</div>
+						<div class="text-center">
+							<button type="submit" class="btn btn-primary text-center px-3 py-2" name="tambah">Tambah</button>
+						</div>
+					</form>
+				</div>
+			</div>	
+		</div>
+	</div>
+</div>
